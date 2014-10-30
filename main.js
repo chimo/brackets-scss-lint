d72613d86850186cacf44ec15e2af18f93b2561e
chimo <chimo@chromic.org>
2014-10-29 21:49:34 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,106 @@
+/*global define: false, brackets: false, $: false, console: false*/
+define(function (require, exports, module) {
+    "use strict";
+
+    var CodeInspection  = brackets.getModule("language/CodeInspection"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
+        NodeDomain      = brackets.getModule("utils/NodeDomain"),
+        ProjectManager  = brackets.getModule("project/ProjectManager"),
+        FileSystem      = brackets.getModule("filesystem/FileSystem"),
+        scssDomain      = new NodeDomain("scss", ExtensionUtils.getModulePath(module, "node/ScssDomain")),
+        yaml = require("js-yaml");
+
+    /**
+     * Asynchronous linting entry point.
+     *
+     * @param {string} text File contents.
+     * @param {string} fullPath Absolute path to the file.
+     *
+     * @return {$.Promise} Promise to return results of code inspection.
+     */
+    function handleHinterAsync(text, fullPath) {
+        var projectRoot = ProjectManager.getProjectRoot().fullPath,
+            def = new $.Deferred(),
+            excludes,
+            exclude,
+            i, len,
+            relFilepath = fullPath.replace(projectRoot, ""), // Path to scss file relative to project root
+            configFileName = projectRoot + ".scss-lint.yml",
+            configContent;
+
+        // Read the configuration file
+        FileSystem.getFileForPath(configFileName).read(function (error, data) {
+            // Print error to console if any
+            if (error) {
+                console.log(error);
+            }
+
+            // Parse config file
+            configContent = yaml.safeLoad(data);
+
+            // Get the list of excluded files
+            excludes = configContent.exclude;
+
+            // Terminate if we're currently looking at an excluded file
+            for (i = 0, len = excludes.length; i < len; i += 1) {
+                exclude = excludes[i];
+
+                if (relFilepath === exclude) {
+                    return;
+                }
+            }
+
+            // Otherwise scss-lint the file
+            scssDomain.exec("build", fullPath, projectRoot)
+                .fail(function (err) {
+                    console.error("[brackets-scss-lint] failed to run scss-lint", err);
+
+                    return err;
+                })
+                .done(function (result) {
+                    var json = JSON.parse(result),
+                        filepath,
+                        errors,
+                        error,
+                        i, len,
+                        results = [],
+                        severity;
+
+                    // Get path (which happens to be the object key)
+                    for (filepath in json) {
+                        break;
+                    }
+
+                    if (filepath === undefined) {
+                        def.resolve(null);
+                    }
+
+                    errors = json[filepath];
+                
+                    for (i = 0, len = errors.length; i < len; i += 1) {
+                        error = errors[i];
+                        severity = (error.severity === "warning") ? CodeInspection.Type.WARNING : CodeInspection.Type.ERROR;
+
+                        results.push({
+                            pos: {
+                                line: error.line - 1,
+                                ch: error.column - 1
+                            },
+                            message: error.reason,
+                            type: severity
+                        });
+                    }
+
+                    def.resolve({errors: results});
+                });
+         });
+
+        return def.promise();
+    }
+
+    CodeInspection.register("scss", {
+        name: "SCSS Lint",
+        scanFileAsync: handleHinterAsync
+    });
+    
+});
---
+++ node/ScssDomain.js
@@ -0,0 +1,69 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, node: true */
+
+(function () {
+    
+    "use strict";
+
+    var exec = require("child_process").exec,
+        fs   = require("fs"),
+        os   = require("os");
+
+    // Run external scss-lint command
+    function cmdBuild(scssFile, projectRoot, callback) {
+
+        var tmpdir          = os.tmpdir() + "/scss-lint",
+            logfile         = projectRoot + "~scss-lint.tmp",
+            _configFileName = projectRoot + ".scss-lint.yml",
+            cmd;
+
+        // The scss-lint gem ( https://rubygems.org/gems/scss-lint ) operates
+        // on directories, so we copy the contents of the current file to
+        // "scss-lint/tmp.scss" in the OS's temporary directory...
+
+        // Create tmp dir if it doesn't exist
+        if (!fs.existsSync(tmpdir)) {
+            fs.mkdir(tmpdir);
+        }
+
+        // Copy file in there
+        // FIXME: error handling
+        fs.createReadStream(scssFile).pipe(fs.createWriteStream(tmpdir + "/tmp.scss"));
+
+        // Build command
+        cmd = "scss-lint -f JSON -c " + _configFileName + " " + tmpdir + " > " + logfile;
+
+        // Call external scss-lint command
+        exec(cmd, function () {
+            fs.readFile(logfile, function (error, data) {
+                // After we're done, delete tmp file
+                fs.unlink(logfile);
+
+                // Pass data to callback
+                callback(error, data.toString());
+            });
+        });
+    }
+
+    // Register "scss" domain
+    function init(domainManager) {
+        if (!domainManager.hasDomain("scss")) {
+            domainManager.registerDomain("scss", {major: 0, minor: 1});
+        }
+
+        domainManager.registerCommand(
+            "scss",
+            "build",
+            cmdBuild,
+            true,
+            "Runs scss linter",
+            ["scssFile", "projectRoot"],
+            [{
+                name: "result",
+                type: "string",
+                description: "The result of the execution"
+            }]
+        );
+    }
+
+    exports.init = init;
+}());
