a83ba77e6bd22337674e8777298022f510cfb82f
chimo <chimo@chromic.org>
2015-01-28 11:52:16 -0500

    We don't need to parse the config file anymore
    Since the changes in 527395b

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ define(function (require, exports, module) {
         NodeDomain      = brackets.getModule("utils/NodeDomain"),
         ProjectManager  = brackets.getModule("project/ProjectManager"),
         FileSystem      = brackets.getModule("filesystem/FileSystem"),
-        scssDomain      = new NodeDomain("scss", ExtensionUtils.getModulePath(module, "node/ScssDomain")),
-        fnmatch         = brackets.getModule("thirdparty/globmatch"),
-        yaml = require("js-yaml");
+        scssDomain      = new NodeDomain("scss", ExtensionUtils.getModulePath(module, "node/ScssDomain"));
 
     /**
      * Asynchronous linting entry point.
@@ -22,78 +20,54 @@ define(function (require, exports, module) {
     function handleHinterAsync(text, fullPath) {
         var projectRoot = ProjectManager.getProjectRoot().fullPath,
             def = new $.Deferred(),
-            excludes,
-            exclude,
-            i, len,
-            relFilepath = fullPath.replace(projectRoot, ""), // Path to scss file relative to project root
-            configFile = projectRoot + ".scss-lint.yml",
-            configContent;
+            configFile = projectRoot + ".scss-lint.yml";
 
-        // Read the configuration file
-        FileSystem.getFileForPath(configFile).read(function (error, data) {
-            if (!error) {
-               // Parse config file
-                configContent = yaml.safeLoad(data);
+            FileSystem.resolve(configFile, function(err) {
+                if (err !== null) {
+                    configFile = null;
+                }
 
-                // Get the list of excluded files
-                excludes = configContent.exclude;
+                scssDomain.exec("build", fullPath, projectRoot, configFile)
+                    .fail(function (err) {
+                        return def.reject(err);
+                    })
+                    .done(function (result) {
+                        var json = JSON.parse(result),
+                            filepath,
+                            errors,
+                            error,
+                            i, len,
+                            results = [],
+                            severity;
 
-                if (excludes) {
-                    // Terminate if we're currently looking at an excluded file
-                    for (i = 0, len = excludes.length; i < len; i += 1) {
-                        exclude = excludes[i];
+                        // Get path (which happens to be the object key)
+                        for (filepath in json) {
+                            break;
+                        }
 
-                        if (fnmatch(relFilepath, exclude)) {
+                        if (filepath === undefined) {
                             return def.resolve(null);
                         }
-                    }
-                }
-            } else {
-                configFile = null;
-            }
 
-            // scss-lint the file if not excluded
-            scssDomain.exec("build", fullPath, projectRoot, configFile)
-                .fail(function (err) {
-                    return def.reject(err);
-                })
-                .done(function (result) {
-                    var json = JSON.parse(result),
-                        filepath,
-                        errors,
-                        error,
-                        i, len,
-                        results = [],
-                        severity;
+                        errors = json[filepath];
 
-                    // Get path (which happens to be the object key)
-                    for (filepath in json) {
-                        break;
-                    }
+                        for (i = 0, len = errors.length; i < len; i += 1) {
+                            error = errors[i];
+                            severity = (error.severity === "warning") ? CodeInspection.Type.WARNING : CodeInspection.Type.ERROR;
 
-                    if (filepath === undefined) {
-                        return def.resolve(null);
-                    }
+                            results.push({
+                                pos: {
+                                    line: error.line - 1,
+                                    ch: error.column - 1
+                                },
+                                message: error.reason,
+                                type: severity
+                            });
+                        }
 
-                    errors = json[filepath];
-
-                    for (i = 0, len = errors.length; i < len; i += 1) {
-                        error = errors[i];
-                        severity = (error.severity === "warning") ? CodeInspection.Type.WARNING : CodeInspection.Type.ERROR;
-
-                        results.push({
-                            pos: {
-                                line: error.line - 1,
-                                ch: error.column - 1
-                            },
-                            message: error.reason,
-                            type: severity
-                        });
-                    }
-
-                    return def.resolve({errors: results});
-                });
-         });
+                        return def.resolve({errors: results});
+                    });
+            });
 
         return def.promise();
     }
