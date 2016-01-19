fb32ab21c047f1512c439faad41a49c668b07cf7
Nicholas Fantozz <nicfanto@usclermg8103.global.publicisgroupe.net>
2016-01-19 13:23:40 -0500

    Updates to use a config file preference.

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ define(function (require, exports, module) {
         ProjectManager  = brackets.getModule("project/ProjectManager"),
         FileSystem      = brackets.getModule("filesystem/FileSystem"),
         PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
-        prefs = PreferencesManager.getExtensionPrefs("scsslint"),
-        gemDir = prefs.get("gemDir") || "",
-        maxExecBuffer = prefs.get("maxExecBuffer") || 200 * 1024,
-        scssDomain      = new NodeDomain("scss", ExtensionUtils.getModulePath(module, "node/ScssDomain"));
+        prefs           = PreferencesManager.getExtensionPrefs("scsslint"),
+        scssDomain      = new NodeDomain("scss", ExtensionUtils.getModulePath(module, "node/ScssDomain")),
+        configFile      = ".scss-lint.yml",
+        execMaxBuffer   = 200 * 1024,
+        gemDir          = "",
+        projectRoot     = null,
+        projectPath     = null;
+    
+    prefs.definePreference("configFile", "string",  configFile);
+    prefs.definePreference("execMaxBuffer", "double", execMaxBuffer);
+    prefs.definePreference("gemDir", "string", gemDir);
 
     /**
      * Asynchronous linting entry point.
@@ -22,56 +29,60 @@ define(function (require, exports, module) {
      * @return {$.Promise} Promise to return results of code inspection.
      */
     function handleHinterAsync(text, fullPath) {
-        var projectRoot = ProjectManager.getProjectRoot().fullPath,
-            def = new $.Deferred(),
-            configFile = projectRoot + ".scss-lint.yml";
+        var def = new $.Deferred();
 
-            FileSystem.resolve(configFile, function(err) {
-                if (err !== null) {
-                    configFile = null;
-                }
+        projectRoot = ProjectManager.getProjectRoot();
+        projectPath = projectRoot ? projectRoot.fullPath : null;
+        configFile = projectPath + prefs.get("configFile");
+        execMaxBuffer = prefs.get("execMaxBuffer");
+        gemDir = prefs.get("gemDir");
 
-                scssDomain.exec("build", fullPath, projectRoot, configFile, gemDir, maxExecBuffer)
-                    .fail(function (err) {
-                        return def.reject(err);
-                    })
-                    .done(function (result) {
-                        var json = JSON.parse(result),
-                            filepath,
-                            errors,
-                            error,
-                            i, len,
-                            results = [],
-                            severity;
+        FileSystem.resolve(configFile, function(err) {
+            if (err !== null) {
+                configFile = null;
+            }
 
-                        // Get path (which happens to be the object key)
-                        for (filepath in json) {
-                            break;
-                        }
+            scssDomain.exec("build", fullPath, projectPath, configFile, gemDir, execMaxBuffer)
+                .fail(function (err) {
+                    return def.reject(err);
+                })
+                .done(function (result) {
+                    var json = JSON.parse(result),
+                        filepath,
+                        errors,
+                        error,
+                        i, len,
+                        results = [],
+                        severity;
 
-                        if (filepath === undefined) {
-                            return def.resolve(null);
-                        }
+                    // Get path (which happens to be the object key)
+                    for (filepath in json) {
+                        break;
+                    }
 
-                        errors = json[filepath];
+                    if (filepath === undefined) {
+                        return def.resolve(null);
+                    }
 
-                        for (i = 0, len = errors.length; i < len; i += 1) {
-                            error = errors[i];
-                            severity = (error.severity === "warning") ? CodeInspection.Type.WARNING : CodeInspection.Type.ERROR;
+                    errors = json[filepath];
 
-                            results.push({
-                                pos: {
-                                    line: error.line - 1,
-                                    ch: error.column - 1
-                                },
-                                message: error.reason,
-                                type: severity
-                            });
-                        }
+                    for (i = 0, len = errors.length; i < len; i += 1) {
+                        error = errors[i];
+                        severity = (error.severity === "warning") ? CodeInspection.Type.WARNING : CodeInspection.Type.ERROR;
 
-                        return def.resolve({errors: results});
-                    });
-            });
+                        results.push({
+                            pos: {
+                                line: error.line - 1,
+                                ch: error.column - 1
+                            },
+                            message: error.reason,
+                            type: severity
+                        });
+                    }
+
+                    return def.resolve({errors: results});
+                });
+        });
 
         return def.promise();
     }
